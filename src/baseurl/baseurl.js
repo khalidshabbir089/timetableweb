72babcc0fac90f56cae8512d47e196d6dd292f9d
khalidshabbir089 <108958530+khalidshabbir089@users.noreply.github.com>
2022-07-19 19:46:07 +0500

    Update baseurl.js

--- src/baseurl/baseurl.js
+++ src/baseurl/baseurl.js
@@ -1,3 +1,3 @@
-export const baseurl = "http://localhost:8002";
+export const baseurl = "https://cuitimetableserver.herokuapp.com";
 // export const baseurl = "https://cuitimetable.herokuapp.com/";
 // "proxy": "https://cuitimetable.herokuapp.com/",
